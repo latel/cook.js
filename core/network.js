d7d22511114e3dc5b92bd7191eccab7ac3d8bf16
latel <latelx64@gmail.com>
2014-06-06 10:46:33 +0800

    core://network:ajax请求的加国添加了自动转换的支持

--- core/network.js
+++ core/network.js
@@ -30,8 +30,19 @@ define(["core://json2"], function () {
         var xmlHttp = init();
         xmlHttp.onreadystatechange = function () {
             if (xmlHttp.readyState=== 4) {
-                xmlHttp.status === 200? 
-                    opt.success(xmlHttp.responseText, xmlHttp.responseXML) : 
+                if (xmlHttp.status === 200) {
+                    var data = xmlHttp.responseText;
+                    if (opt.type && opt.type !== "string") {
+                        switch (opt.type) {
+                            case "json":
+                                data = JSON.parse(data);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    opt.success(data, xmlHttp.responseXML);
+                } else
                     opt.error(xmlHttp.responseText, xmlHttp.status);
             }
         }
@@ -53,4 +64,5 @@ define(["core://json2"], function () {
  * 2014/03/12   0.0.1   模块创建
  * 2014/03/26   0.0.1   修复了IE6下的不兼容
  * 2014/04/12   0.0.1   修复了data为空时会发送一个undefined字符串的严重问题
+ * 2014/06/06   0.0.1   添加了指定返回type的支持
  */
